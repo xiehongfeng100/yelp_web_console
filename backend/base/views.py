46d3085ccdbafa6428c05bb3827fe0a77877765d
xiehongfeng100 <xiehongfeng100@yeah.net>
2018-07-11 08:11:03 +0800

    change views original inherited mixins/viewset to ReadOnlyModelViewset

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,55 +1,43 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from rest_framework import viewsets, mixins
+from rest_framework import viewsets
 
 import models as custom_models
 import serializers as custom_serializers
 
 
-class UserViewSet(mixins.RetrieveModelMixin,
-                  mixins.ListModelMixin,
-                  viewsets.GenericViewSet):
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.User.objects.all()
     serializer_class = custom_serializers.UserSerializer
     filter_fields = ()
 
 
-class FriendViewSet(mixins.RetrieveModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
+class FriendViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.Friend.objects.all()
     serializer_class = custom_serializers.FriendSerializer
     filter_fields = ()
 
 
-class BusinessViewSet(mixins.RetrieveModelMixin,
-                      mixins.ListModelMixin,
-                      viewsets.GenericViewSet):
+class BusinessViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.Business.objects.all()
     serializer_class = custom_serializers.BusinessSerializer
     filter_fields = ()
 
 
-class ReviewViewSet(mixins.RetrieveModelMixin,
-                    mixins.ListModelMixin,
-                    viewsets.GenericViewSet):
+class ReviewViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.Review.objects.all()
     serializer_class = custom_serializers.ReviewSerializer
     filter_fields = ()
 
 
-class CheckInViewSet(mixins.RetrieveModelMixin,
-                     mixins.ListModelMixin,
-                     viewsets.GenericViewSet):
+class CheckInViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.CheckIn.objects.all()
     serializer_class = custom_serializers.CheckInSerializer
     filter_fields = ()
 
 
-class PhotoViewSet(mixins.RetrieveModelMixin,
-                   mixins.ListModelMixin,
-                   viewsets.GenericViewSet):
+class PhotoViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = custom_models.Photo.objects.all()
     serializer_class = custom_serializers.PhotoSerializer
-    filter_fields = ('id',)
+    filter_fields = ()
